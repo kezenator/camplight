a21afb23624e96732b3f566a3151c8cf472d6eb7
Kezenator <github@kezenator.com>
2018-12-10 21:08:00 +1000

    Added missing files from last commit

---
+++ src/Applications/MiamiNightsWeb/ts/ui/logo/LogoCommon.ts
@@ -0,0 +1,66 @@
+﻿/// <reference path="../../util/Math.ts" />
+
+namespace ui.logo
+{
+    export class LogoCommon
+    {         
+        public static PERIOD_WASH: number = 10000;    
+        public static DURATION_FADE: number = 1500;  
+
+        public static drawPressStart(ctx: CanvasRenderingContext2D, ms: number, buttons: ui.Buttons)
+        {                                                                                          
+            var on = (ms % 1000) >= 500;
+
+            if (on)
+            {
+                var text = 'PRESS PLAY';
+
+                ctx.font = '65px "KarmaticArcade"';
+
+                var metrics = ctx.measureText(text);
+
+                var x = 960 - 0.5 * metrics.width;
+                var y = 1050;
+
+                var grad = ctx.createLinearGradient(x, y, x + metrics.width, y);
+                grad.addColorStop(0, 'hsl(0,100%,50%)');
+                grad.addColorStop(1 / 5, 'hsl(30,100%,50%)');
+                grad.addColorStop(2 / 5, 'hsl(60,100%,50%)');
+                grad.addColorStop(3 / 5, 'hsl(120,100%,50%)');
+                grad.addColorStop(4 / 5, 'hsl(180,100%,50%)');
+                grad.addColorStop(1, 'hsl(240,100%,50%)');
+
+                ctx.fillStyle = grad;
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 6;
+
+                ctx.strokeText(text, x, y);
+                ctx.fillText(text, x, y);
+
+                buttons.setPlayColor('#ffffff');
+            }
+            else
+            {
+                buttons.setPlayColor('#000000');
+            }
+        }
+
+        public static colorWaveButtons(ms: number, alpha: number, buttons: ui.Buttons)
+        {
+            var lightness = 50 * (1 - alpha);
+
+            for (var i = 0; i < Buttons.NUMBER; ++i)
+            {
+                var base = 360 * ((ms % 4000) / 4000);
+                var offset = i * 60;
+                var hue = Math.floor((base + offset) % 360);
+
+                buttons.setButtonColor(i, 'hsl(' + hue + ',100%,' + lightness + '%)');
+            }
+
+            var wash_hue = 360 * (ms % LogoCommon.PERIOD_WASH) / LogoCommon.PERIOD_WASH;
+
+            buttons.setWashColor('hsl(' + wash_hue + ',100%,' + lightness + '%)');
+        }
+    }
+}
---
+++ src/Applications/MiamiNightsWeb/ts/ui/logo/VideoScreen.ts
@@ -0,0 +1,80 @@
+﻿/// <reference path="../CanvasScreen.ts" />
+/// <reference path="../../util/Math.ts" />
+/// <reference path="LogoCommon.ts" />
+
+
+namespace ui.logo
+{
+    export class VideoScreen extends CanvasScreen
+    {
+        private video: HTMLVideoElement;
+        private instagram: HTMLImageElement;
+
+        public constructor(app: App)
+        {
+            super(app);
+
+            this.video = document.createElement('video');
+            this.video.classList.add('video');
+            this.video.volume = 0;
+            this.video.width = 1920;
+            this.video.height = 1080;
+            this.video.src = '/res/video/MiamiNights1984-Accelerated.mp4';
+
+            this.getDiv().appendChild(this.video);
+
+            this.instagram = document.createElement('img');
+            this.instagram.src = '/res/imgs/instagram.png';
+        }
+
+        public show()
+        {
+            super.show();
+
+            this.video.play();
+
+            this.getApp().getButtons().setBackColor('#000000');
+        }
+
+        public hide()
+        {
+            super.hide();
+
+            this.video.pause();
+        }
+
+        public draw(ctx: CanvasRenderingContext2D, ms: number): void
+        {
+            if (ms > 30000)
+            {
+                this.getApp().showScreen(App.LOGO);
+                return;
+            }
+
+            var buttons = this.getButtons();
+
+            ctx.clearRect(0, 0, 1920, 1080);
+
+            var alpha = 0;
+
+            if (ms < LogoCommon.DURATION_FADE)
+            {
+                alpha = 1 - (ms / LogoCommon.DURATION_FADE);
+            }
+            else if (ms > (30000 - LogoCommon.DURATION_FADE))
+            {
+                alpha = (ms - (30000 - LogoCommon.DURATION_FADE)) / LogoCommon.DURATION_FADE;
+            }
+
+            LogoCommon.drawPressStart(ctx, ms, buttons);
+            LogoCommon.colorWaveButtons(ms, alpha, buttons);
+            ctx.drawImage(this.instagram, 1421, 952);
+
+            if (alpha != 0)
+            {
+                ctx.fillStyle = 'rgba(0,0,0,' + alpha + ')';
+                ctx.fillRect(0, 0, 1920, 1080);
+            }
+        }
+    }
+}
